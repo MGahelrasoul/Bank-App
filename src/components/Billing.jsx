2b83eb9e578901c9896e1392e9247925020972ab
Mohammed Gahelrasoul <M97gahelrasoul@gmail.com>
2023-11-23 11:42:40 -0500

    deploying

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+
+      <div className="absolute z-[3] -left-[80%] -top-[30%] w-[100%] h-[100%] rounded-full white__gradient  opacity-[0.2]" />
+      <div className="absolute z-[0] -left-[80%] -bottom-[30%] w-[100%] h-[100%] rounded-full pink__gradient  opacity-[0.1]" />
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" />
+        billing & invoicing.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Master your finances. Take control of billing and invoicing with
+        user-friendly tools, making financial management a breeze. Experience
+        simplicity at its best.
+      </p>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img
+          src={apple}
+          alt="apple store"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google store"
+          className="w-[128px] h-[42px] object-contain cursor-pointer"
+        />
+      </div>
+    </div>
+  </section>
+)
 
 export default Billing
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px] cursor-pointer" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -16,7 +16,7 @@ const Navbar = () => {
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
               index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
-            } text-white`}
+            } text-white hover:text-secondary`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
@@ -34,7 +34,7 @@ const Navbar = () => {
         <div
           className={`${
             toggle ? 'flex' : 'hidden'
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-discount-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav, index) => (
@@ -44,9 +44,7 @@ const Navbar = () => {
                   index === navLinks.length - 1 ? 'mb-0' : 'mb-4'
                 } text-white`}
               >
-                <a href={`#${nav.id}`}>
-                  {nav.title}
-                  </a>
+                <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
           </ul>
